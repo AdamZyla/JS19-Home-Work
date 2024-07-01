e5ac4bb477a1c3416e51cf52396c4fea32f07328
AdamZyla <azyla@interia.pl>
2024-07-02 00:17:33 +0200

    create repo JS 19 hw 1-3

---
+++ task-1.js
@@ -0,0 +1,12 @@
+function makeTransaction(quantity, pricePerDroid) {
+    const totalPrice = quantity * pricePerDroid;
+    return `You ordered ${quantity} droids worth ${totalPrice} credits!`;
+  }
+  
+  // Testowanie funkcji
+  console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
+  console.log(makeTransaction(3, 4000)); // "You ordered 3 droids worth 12000 credits!"
+  console.log(makeTransaction(7, 1500)); // "You ordered 7 droids worth 10500 credits!"
+  
+
+    
