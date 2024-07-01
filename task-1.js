9e4968276ec9464cb5c16a7155926ccb2033a896
AdamZyla <azyla@interia.pl>
2024-07-02 00:51:26 +0200

    work in progres ;)

--- task-1.js
+++ task-1.js
@@ -7,4 +7,7 @@ function makeTransaction(quantity, pricePerDroid) {
   console.log(makeTransaction(5, 3000)); // "You ordered 5 droids worth 15000 credits!"
   console.log(makeTransaction(3, 4000)); // "You ordered 3 droids worth 12000 credits!"
   console.log(makeTransaction(7, 1500)); // "You ordered 7 droids worth 10500 credits!"
+
+// Testowanie  dowolnych wartości
+  console.log(makeTransaction(10, 5000)); // "You ordered 7 droids worth 10500 credits!"
   
