f955948897e9f4ddc6fa69601f3d36a4d68da47b
AdamZyla <azyla@interia.pl>
2024-07-05 18:46:45 +0200

    task-3

--- task-2.js
+++ task-2.js
@@ -6,6 +6,10 @@
     return `"Shipping to ${country} will cost ${totalPrice} credits"`;
     }
 
+    // Testowanie funkcji
     console.log(getShippingMessage("Australia", 120, 50)); // "Shipping to Australia will cost 170 credits"
     console.log(getShippingMessage("Germany", 80, 20)); // "Shipping to Germany will cost 100 credits"
-    console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits"   
+    console.log(getShippingMessage("Sweden", 100, 20)); // "Shipping to Sweden will cost 120 credits" 
+    
+    // Testowanie  dowolnych wartości
+    console.log(getShippingMessage("Brasil", 150, 100)); // "Shipping to Australia will cost 170 credits"
---
+++ task-3.js
@@ -0,0 +1,24 @@
+
+
+function getElementWidth(content, padding, border) {
+
+    function parsePx(value) {
+        return parseFloat(value.replace('px', ''));
+}
+
+    const contentWidth = parsePx(content);
+    const paddingWidth = parsePx(padding);
+    const borderWidth = parsePx(border);
+
+    const totalWidth = contentWidth + 2 * paddingWidth + 2 * borderWidth;
+
+    return totalWidth;
+}
+// Testowanie funkcji
+console.log(getElementWidth("50px", "8px", "4px"));
+console.log(getElementWidth("60px", "12px", "8.5px"));
+console.log(getElementWidth("200px", "0px", "0px"));
+
+
+// Testowanie  dowolnych wartości
+console.log(getElementWidth("500px", "50px", "50px"));
